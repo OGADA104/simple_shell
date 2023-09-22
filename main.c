42cd5bb5220fe2d8d9681d0c4ac477d1106f685f
MICHAEL <michaelogada104@gmail.com>
2023-09-22 08:09:37 +0000

    reworked shell project

--- main.c
+++ main.c
@@ -1,20 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include "main.h"
-
-#define BUFFER_SIZE 1024
+#include "shell.h"
 
 /**
- * main - Entry point of the simple shell program
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: 0 on success
+ * Return: 0 on success, 1 on error
  */
-int main(void)
+int main(int ac, char **av)
 {
-	process_input();
-	return (0);
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
